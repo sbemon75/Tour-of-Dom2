281d325ba63be13ac6a729a5b7c265cd51500aa4
sbemon75 <sunyeat4623@gmail.com>
2025-01-19 00:21:14 +0600

    my third commit

--- DOM Event/event.js
+++ DOM Event/event.js
@@ -1,10 +1,17 @@
 console.log('Dom ar ajka special event');
 
+// option:1
+// <button onclick="console.log(10)">Click me</button>
+
+
+
+// option:2
 function makegreen() {
     document.body.style.backgroundColor = 'green'
 }
 
 
+// option:3
 const makeblue = document.getElementById('make-blue');
 makeblue.onclick = makeblue1;
 
