7c6c79a7a5f1b2fc8e22ccc13750cd1b4e4399a0
sbemon75 <sunyeat4623@gmail.com>
2025-01-18 23:57:24 +0600

    my second commit

--- DOM Event/event.js
+++ DOM Event/event.js
@@ -3,3 +3,11 @@ console.log('Dom ar ajka special event');
 function makegreen() {
     document.body.style.backgroundColor = 'green'
 }
+
+
+const makeblue = document.getElementById('make-blue');
+makeblue.onclick = makeblue1;
+
+function makeblue1 () {
+    document.body.style.backgroundColor = 'blue';
+}
